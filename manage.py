39978a11bbc0d8f7b742961620661536b4a9d3d9
Hassan <hassansmallah@gmail.com>
2022-01-03 12:40:14 +0300

    app.config

--- manage.py
+++ manage.py
@@ -1,10 +1,29 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+import environ  # for .env
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 app = Flask(__name__)
+
+username = env('username')
+password = env('password')
+
+# app config
+app.config['MONGO_URI'] = 'mongodb+srv://{}:{}@cluster0.rthfj.mongodb.net/test?retryWrites=true&w=majority'.format(
+    username, password)
+app.config['MONGO_DBNAME'] = 'test'
+
+# connect to mongo db
+mongo = PyMongo(app)
+
 
 @app.route('/', methods=['GET'])
 def index():
     return 'Hello to pymongo flask api :)'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
